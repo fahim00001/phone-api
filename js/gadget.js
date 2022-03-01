c620063f5e83755254b08e76a3f88eb779cb2181
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-03-02 01:11:24 +0600

    phone feature added half

--- js/gadget.js
+++ js/gadget.js
@@ -15,6 +15,7 @@ const displaySearchResults = phones => {
     phones.forEach(phone => {
         const div = document.createElement('div');
         const phoneIdName = phone.slug;
+        console.log(phoneIdName);
         div.classList.add('col');
         div.innerHTML = `
     <div class="card">
@@ -41,5 +42,25 @@ const loadPhoneDetails = phoneId => {
 }
 
 const displayloadDetails = phone => {
-    console.log(phone);
+    const phoneFeature = document.getElementById('phone-details');
+    phoneFeature.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('row', 'container', 'mx-auto', 'gx-5', 'my-5')
+    div.innerHTML = `
+    
+    <div class="col-lg-5">
+        <div class="p-3 mx-auto  bg-light">
+        <img  src="${phone.image}" class=" w-75 ps-5 mx-3  mt-2" alt="...">
+        <h6 class="text-center my-3">${phone.releaseDate}</h6>
+        </div>
+    </div>
+    <div class="col-lg-7">
+        <div class="p-3 border bg-light">
+        <p class="text-center fw-bold my-1">Phone Feature</p>
+        
+        </div>
+    </div>
+  
+        `
+    phoneFeature.appendChild(div);
 }
